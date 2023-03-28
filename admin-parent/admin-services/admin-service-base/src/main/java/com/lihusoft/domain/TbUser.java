0f90c7b8571617d3baef2488c418cf3cd7ec7df9
tsc <458730548@qq.com>
2023-03-28 11:52:10 +0800

    mybatis-generate自动生成插件生成实体类

---
+++ admin-parent/admin-services/admin-service-base/src/main/java/com/lihusoft/domain/TbUser.java
@@ -0,0 +1,81 @@
+package com.lihusoft.domain;
+
+import java.util.Date;
+
+public class TbUser {
+    private Long id;
+
+    private String name;
+
+    private String loginName;
+
+    private String password;
+
+    private Date createTime;
+
+    private Date lastUpdateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", loginName=").append(loginName);
+        sb.append(", password=").append(password);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", lastUpdateTime=").append(lastUpdateTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
