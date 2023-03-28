d08d3859fe00faed766f1544fb94051093fe7d21
tsc <458730548@qq.com>
2023-03-28 18:11:19 +0800

    登陆界面编写

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,35 +1,16 @@
 <template>
-  <a-row type="flex">
-    <a-col flex="100px">
-      100px
-      <a-space style="width: 100%">
-        <a-button type="primary" loading>Loading</a-button>
-        <a-button type="primary" size="small" loading>Loading</a-button>
-      </a-space>
-    </a-col>
-    <a-col flex="auto">auto</a-col>
-  </a-row>
+  <TheLogin></TheLogin>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import {defineComponent} from 'vue';
+import TheLogin from "@/components/the-login.vue";
 
-#nav {
-  padding: 30px;
-}
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheLogin
+  },
+});
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
