9e63546ddc1748675902b6ce75f73f24b5288275
tsc <458730548@qq.com>
2023-03-29 17:45:01 +0800

    登陆页面优化

--- front/src/components/the-login.vue
+++ front/src/components/the-login.vue
@@ -2,17 +2,22 @@
   <div class="container">
     <div class="container-login">
       <a-card :bordered="false">
+        <a class="trans" @click="exchangeScan()">
+          <QrcodeOutlined>
+          </QrcodeOutlined>
+          <div class="imgApp"></div>
+        </a>
         <template v-slot:title>
           <h2 class="container-login-title">登陆</h2>
         </template>
-        <a-row type="flex">
+        <a-row type="flex" style="padding-top: 0;height: 300px">
           <a-col flex="280px">
-            <a-image style="height: 300px"
+            <a-image style="height: 280px"
                      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
             />
           </a-col>
           <a-col flex="auto">
-            <a-card title="" :bordered="false" style="width: 100% ; height:100%; padding-top: 30px">
+            <a-card title="" :bordered="false" style="width: 100% ; height:100%;">
               <a-form
                   :model="formState"
                   name="normal_login"
@@ -20,6 +25,7 @@
                   @finish="onFinish"
                   @finishFailed="onFinishFailed"
                   v-bind="layout"
+                  v-show="!isShowScan"
               >
                 <a-form-item
                     label="用户名"
@@ -50,14 +56,39 @@
                   <a class="login-form-forgot" href="">&nbsp;忘记密码？</a>
                 </a-form-item>
 
-                <div style="text-align: center">
+                <a-form-item style="text-align: center" :wrapper-col="{ span: 22, offset: 2 }">
                   <a-button :disabled="disabled" type="primary" html-type="submit"
-                            style="width: 180px"
+                            style="width: 60%; text-align: center"
                             class="login-form-button">
                     登陆
                   </a-button>
-                  <span><a href="">&nbsp;&nbsp;前往注册!</a></span>
+                  <a class="login-form-forgot" style="margin-left: 10px" href="">&nbsp;前往注册？</a>
+                </a-form-item>
+
+                <div>
+                  <a>
+                    <WechatOutlined class="wechart-svg"/>
+                    <span style="vertical-align: middle">&nbsp;微信登陆</span>
+                  </a>
+                  <a>
+                    <QqOutlined class="qq-svg"/>
+                    <span style="vertical-align: middle">&nbsp;qq登陆</span>
+                  </a>
                 </div>
+              </a-form>
+              <a-form
+                  :model="formState"
+                  name="normal_login"
+                  class="login-form"
+                  @finish="onFinish"
+                  @finishFailed="onFinishFailed"
+                  v-bind="layout"
+                  v-show="isShowScan"
+              >
+                <a-form-item :wrapper-col="{ span: 20, offset: 4  }">
+                  <QrcodeOutlined class="scan-svg">
+                  </QrcodeOutlined>
+                </a-form-item>
               </a-form>
             </a-card>
           </a-col>
@@ -69,8 +100,8 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive, computed} from 'vue';
-import {UserOutlined, LockOutlined} from '@ant-design/icons-vue';
+import {defineComponent, reactive, computed, ref} from 'vue';
+import {UserOutlined, LockOutlined, WechatOutlined, QqOutlined, QrcodeOutlined} from '@ant-design/icons-vue';
 
 interface FormState {
   loginName: string;
@@ -83,8 +114,13 @@ export default defineComponent({
   components: {
     UserOutlined,
     LockOutlined,
+    WechatOutlined,
+    QqOutlined,
+    QrcodeOutlined
   },
   setup() {
+    const isShowScan = ref(false)
+
     const formState = reactive<FormState>({
       loginName: '',
       password: '',
@@ -93,7 +129,7 @@ export default defineComponent({
 
     const layout = {
       labelCol: {span: 5},
-      wrapperCol: {span: 18},
+      wrapperCol: {span: 19},
     };
 
     const onFinish = (values: any) => {
@@ -106,12 +142,18 @@ export default defineComponent({
     const disabled = computed(() => {
       return !(formState.loginName && formState.password);
     });
+    const exchangeScan = () => {
+      isShowScan.value = !isShowScan.value
+    }
+
     return {
       layout,
       formState,
       onFinish,
       onFinishFailed,
       disabled,
+      exchangeScan,
+      isShowScan
     };
   },
 });
@@ -127,8 +169,8 @@ export default defineComponent({
   height: 100%;
   width: 100%;
   /*background-color: black;*/
-  padding: 80px 0px;
-  background-color: #eef;
+  padding: 100px 0;
+  background-color: cornflowerblue;
 }
 
 .container-login {
@@ -142,5 +184,49 @@ export default defineComponent({
 
 .container-login-title {
   text-align: center;
+  height: 20px;
+  line-height: 30px;
 }
+
+.wechart-svg svg, .qq-svg svg {
+  width: 25px;
+  height: 25px;
+  margin-left: 10px;
+  vertical-align: middle;
+}
+
+.trans svg {
+  position: absolute;
+  right: 0;
+  width: 64px;
+  height: 64px;
+  /*background-color: #2b96e6;*/
+  top: 0;
+}
+
+.imgApp {
+  width: 64px;
+  height: 64px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  background: linear-gradient(225deg, transparent 45px, #fff 0);
+}
+
+.container-login .ant-card {
+  border-radius: 10px;
+}
+
+.container-login .ant-card-body {
+  padding: 15px;
+}
+
+.scan-svg svg {
+  height: 250px;
+  line-height: 250px;
+  width: 250px;
+  margin: 0 auto;
+  vertical-align: middle;
+}
+
 </style>
